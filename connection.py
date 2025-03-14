d9024d05123c1def232ce99bcc81f0f337e14035
michaelv2006 <michaelvargas122006@gmail.com>
2025-03-14 15:51:15 -0500

    cambio de url de base de datos

--- connection.py
+++ connection.py
@@ -3,7 +3,7 @@ from pymongo.server_api import ServerApi
 
 
 def connect_to_mongodb(db_name, collection_name):
-    uri = "mongodb+srv://mardugo:$Hola123.@sampleinformationservic.t2yog.mongodb.net/?retryWrites=true&w=majority&appName=SampleInformationService"
+    uri = "mongodb+srv://michaelvargas122006:radiante_mic@ifmer.bos2u.mongodb.net/?retryWrites=true&w=majority&appName=ifmer"
     client = MongoClient(uri, server_api=ServerApi('1'))
     db = client[db_name]
     collection = db[collection_name]
