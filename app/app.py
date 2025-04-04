96dceba801be5339b34003c7b6e87375df1440e5
michaelv2006 <michaelvargas122006@gmail.com>
2025-04-03 22:23:14 -0500

    get patient by identifier

--- app/app.py
+++ app/app.py
@@ -23,16 +23,6 @@ async def get_patient_by_id(patient_id: str):
     else:
         raise HTTPException(status_code=500, detail=f"Internal error. {status}")
 
-
-@app.post("/patient", response_model=dict)
-async def add_patient(request: Request):
-    new_patient_dict = dict(await request.json())
-    status,patient_id = WritePatient(new_patient_dict)
-    if status=='success':
-        return {"_id":patient_id}  # Return patient id
-    else:
-        raise HTTPException(status_code=500, detail=f"Validating error: {status}")
-
 @app.get("/patient", response_model=dict)
 async def get_patient_by_identifier(system: str, value: str):
     print("solicitud datos:",system,value)
@@ -43,6 +33,18 @@ async def get_patient_by_identifier(system: str, value: str):
         raise HTTPException(status_code=204, detail="Patient not found")
     else:
         raise HTTPException(status_code=500, detail=f"Internal error. {status}")
+
+
+@app.post("/patient", response_model=dict)
+async def add_patient(request: Request):
+    new_patient_dict = dict(await request.json())
+    status,patient_id = WritePatient(new_patient_dict)
+    if status=='success':
+        return {"_id":patient_id}  # Return patient id
+    else:
+        raise HTTPException(status_code=500, detail=f"Validating error: {status}")
+
+
 
 if __name__ == '__main__':
     import uvicorn
