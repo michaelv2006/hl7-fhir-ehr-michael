9442d7535f135994f754a348ee24d32dd1d97063
michaelv2006 <michaelvargas122006@gmail.com>
2025-04-06 00:02:27 -0500

    Update ServiceRequestcrud.py

--- app/controlador/ServiceRequestcrud.py
+++ app/controlador/ServiceRequestcrud.py
@@ -4,7 +4,7 @@ from fhir.resources.patient import Patient
 import json
 
 collection = connect_to_mongodb("SamplePatientService", "patients")
-service_requests_collection = connect_to_mongodb("SamplePatientService", "Procedures")
+service_requests_collection = connect_to_mongodb("SamplePatientService", "services-request")
 
 def GetPatientById(patient_id: str):
     try:
