5571be51040b46972e36486dcc3946854d9521fe
michaelv2006 <michaelvargas122006@gmail.com>
2025-04-05 23:13:36 -0500

    Update app.py

--- app/app.py
+++ app/app.py
@@ -7,7 +7,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://medservice-94k9.onrender.com/servicerequest", "https://hl7-fhir-ehr-michael.onrender.com "],  # Permitir solo este dominio
+    allow_origins=["https://hl7-fhir-ehr-michael.onrender.com/patient", "https://hl7-fhir-ehr-michael.onrender.com "],  # Permitir solo este dominio
     allow_credentials=True,
     allow_methods=["*"],  # Permitir todos los métodos (GET, POST, etc.)
     allow_headers=["*"],  # Permitir todos los encabezados
