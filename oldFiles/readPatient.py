1e533e45e9bff0cfbd8d94f0872364fe840529d0
michaelv2006 <michaelvargas122006@gmail.com>
2025-04-06 01:09:42 -0500

    Update readPatient.py

--- oldFiles/readPatient.py
+++ oldFiles/readPatient.py
@@ -40,7 +40,7 @@ def display_patients(patient_list):
 # Ejemplo de uso
 if __name__ == "__main__":
     # Cadena de conexión a MongoDB (reemplaza con tu propia cadena de conexión)
-    uri = "mongodb+srv://michaelvargas122006:Interior5-417@ifmer.bos2u.mongodb.net/?retryWrites=true&w=majority&appName=ifmer"
+    uri = "mongodb+srv://michaelvargas122006:ltL0cs36V8iO38Hg@ifmer.bos2u.mongodb.net/?retryWrites=true&w=majority&appName=ifmer"
 
     # Nombre de la base de datos y la colección
     db_name = "SamplePatientService"
