76290561fdd267e3c4100498410f32e3d6dd077a
michaelv2006 <michaelvargas122006@gmail.com>
2025-04-11 11:54:17 -0500

    Update app.py

--- app/app.py
+++ app/app.py
@@ -44,11 +44,6 @@ async def add_patient(request: Request):
     else:
         raise HTTPException(status_code=500, detail=f"Validating error: {status}")
 
-@app.get("/")
-def root():
-    return {"status": "ok"}
-
-
 if __name__ == '__main__':
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=10000)
