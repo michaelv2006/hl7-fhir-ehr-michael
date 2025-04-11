da86863954fee7b433f7438c1abc3ae62dc38875
michaelv2006 <michaelvargas122006@gmail.com>
2025-04-11 18:01:14 -0500

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,13 +1,13 @@
 from fastapi import FastAPI, HTTPException, Request
 import uvicorn
-from app.controlador.PatientCrud import GetPatientById,WritePatient,GetPatientByIdentifier,read_service_request,WriteServiceRequest
+from app.controlador.PatientCrud import GetPatientById,WritePatient,GetPatientByIdentifier
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://hl7-patient-write-michael.onrender.com","https://write-service-request-mlbj.onrender.com"],  # Permitir solo este dominio
+    allow_origins=["https://hl7-patient-write-michael.onrender.com"],  # Permitir solo este dominio
     allow_credentials=True,
     allow_methods=["*"],  # Permitir todos los métodos (GET, POST, etc.)
     allow_headers=["*"],  # Permitir todos los encabezados
@@ -44,27 +44,6 @@ async def add_patient(request: Request):
     else:
         raise HTTPException(status_code=500, detail=f"Validating error: {status}")
 
-@app.get("/service-request/{service_request_id}", response_model=dict)
-async def get_service_request(service_request_id: str):
-    # Llama a la función auxiliar que obtiene la solicitud de servicio
-    service_request = read_service_request(service_request_id)
-    if service_request:
-        return service_request
-    else:
-        raise HTTPException(status_code=404, detail="Solicitud de servicio no encontrada")
-
-@app.post("/service-request", response_model=dict)
-async def add_service_request(request: Request):
-    # Obtiene el JSON enviado en la solicitud POST
-    service_request_data = await request.json()
-    
-    # Llama a la función que inserta la solicitud en la base de datos
-    status, service_request_id = WriteServiceRequest(service_request_data)
-    
-    if status == "success":
-        return {"_id": service_request_id}
-    else:
-        raise HTTPException(status_code=500, detail=f"Error al registrar la solicitud: {status}")
 
 if __name__ == '__main__':
     import uvicorn
