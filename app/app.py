9b87c7accc9e700161c9d8732987679cb1afb0ad
michaelv2006 <michaelvargas122006@gmail.com>
2025-04-11 11:42:14 -0500

    Update app.py

--- app/app.py
+++ app/app.py
@@ -46,4 +46,4 @@ async def add_patient(request: Request):
 
 if __name__ == '__main__':
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=10000)
