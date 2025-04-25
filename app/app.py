d9dce2e8bbfb5b57fde26e338d4775f961691bbd
michaelv2006 <michaelvargas122006@gmail.com>
2025-04-24 21:33:16 -0500

    Update app.py

--- app/app.py
+++ app/app.py
@@ -44,6 +44,28 @@ async def add_patient(request: Request):
     else:
         raise HTTPException(status_code=500, detail=f"Validating error: {status}")
 
+@app.get("/service-request/{service_request_id}", response_model=dict)
+async def get_service_request(service_request_id: str):
+    # Llama a la función auxiliar que obtiene la solicitud de servicio
+    service_request = read_service_request(service_request_id)
+    if service_request:
+        return service_request
+    else:
+        raise HTTPException(status_code=404, detail="Solicitud de servicio no encontrada")
+
+@app.post("/service-request", response_model=dict)
+async def add_service_request(request: Request):
+    # Obtiene el JSON enviado en la solicitud POST
+    service_request_data = await request.json()
+    
+    # Llama a la función que inserta la solicitud en la base de datos
+    status, service_request_id = WriteServiceRequest(service_request_data)
+    
+    if status == "success":
+        return {"_id": service_request_id}
+    else:
+        raise HTTPException(status_code=500, detail=f"Error al registrar la solicitud: {status}")
+
 
 if __name__ == '__main__':
     import uvicorn
