cfef63ed57ab86b8c6c6f955d6c52de44f17958c
michaelv2006 <michaelvargas122006@gmail.com>
2025-04-26 20:06:03 -0500

    Update PatientCrud.py

--- app/controlador/PatientCrud.py
+++ app/controlador/PatientCrud.py
@@ -5,6 +5,7 @@ import json
 
 collection = connect_to_mongodb("SamplePatientService", "patients")
 service_requests_collection = connect_to_mongodb("SamplePatientService", "service_requests")
+appointment_collection = connect_to_mongodb("SamplePatientService", "appointment")
 
 def GetPatientById(patient_id: str):
     try:
