17a52c33367db8862997be0562e0e57a4d8724c8
michaelv2006 <michaelvargas122006@gmail.com>
2025-05-20 18:36:04 -0500

    Update app.py

--- app/app.py
+++ app/app.py
@@ -35,14 +35,18 @@ async def get_patient_by_identifier(system: str, value: str):
         raise HTTPException(status_code=500, detail=f"Internal error. {status}")
 
 
-@app.post("/patient", response_model=dict)
-async def add_patient(request: Request):
-    new_patient_dict = dict(await request.json())
-    status,patient_id = WritePatient(new_patient_dict)
-    if status=='success':
-        return {"_id":patient_id}  # Return patient id
+@app.post("/service-request", response_model=dict)
+async def add_service_request(request: Request):
+    service_request_data = await request.json()
+    status, service_request_id = WriteServiceRequest(service_request_data)
+
+    if status == "success":
+        return {"_id": service_request_id}
+    elif status == "alreadyExists":
+        raise HTTPException(status_code=409, detail=f"La solicitud de servicio ya existe. ID: {service_request_id}")
     else:
-        raise HTTPException(status_code=500, detail=f"Validating error: {status}")
+        raise HTTPException(status_code=500, detail=f"Error al registrar la solicitud: {status}")
+
 
 @app.get("/service-request/{service_request_id}", response_model=dict)
 async def get_service_request(service_request_id: str):
